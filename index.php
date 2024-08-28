2183c4a8e44575baf990134c73a581383a3d4223
badik2001 <138158222+badik2001@users.noreply.github.com>
2024-08-28 17:22:46 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-header('Set-Cookie: foo=42');
+//setcookie('username', 'admin');
+//setcookie('secret', sha1('13.04.1980'));
+$users = require_once __DIR__ . '/users.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,12 +13,33 @@ header('Set-Cookie: foo=42');
 <body>
     
     <h1>"hello"</h1>
+    <p><b>Функция суммы 10 и 20</b></p>
+    <?php 
+        include __DIR__ . '/functions.php';
+        echo sum(10, 20);
+    ?>
+    <p><b>Форма запроса для отправки картинки и обработки в upload.php  и сохранение картинки в папку pictures</b></p>
     <form action="upload.php" method="post" enctype="multipart/form-data">
 
         <input type='file' name="picture">
         <button type="submit">Отправить</button>
-
     </form>
 
+    <?php
+    echo time();
+    ?><br>
+
+    <a href="test.php">TEST</a><br>
+
+    <?php
+    //$password = '123456';
+    //echo password_hash($password, PASSWORD_DEFAULT);
+
+    $db = '$2y$10$WYOCpEKmu6AlL6e8qL8xru1OUrPm2YqpKEVpHQiY1VdnGXmAiPX6W';
+    $password = '123456';
+
+
+    var_dump(password_verify($password, $db));
+    ?>
 </body>
 </html>
